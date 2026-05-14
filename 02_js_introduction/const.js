1d2561cc75c27e0a1ed55b84a324679e839c29ee
prince <princenandoliya@gmail.com>
2026-05-14 14:23:55 +0530

    added

---
+++ 02_js_introduction/const.js
@@ -0,0 +1,21 @@
+
+
+function name1(){
+
+    const  name="prince";
+
+    if(true){
+        console.log("My name is",name);
+    }
+}
+
+name1();
+
+// declaration and initialization
+const name = "prince"
+console.log(name)
+
+name = "ankit"
+console.log(name)
+
+
---
+++ 03_data_type/data_type.js
@@ -0,0 +1,29 @@
+
+//primitive data type
+
+//1.string
+let all = "prince"
+
+console.log(all)
+console.log(typeof all)
+
+//2.number
+let num = 1234563
+
+console.log(num)
+console.log(typeof num)
+
+
+
+//4.Boolean
+let T = true
+
+console.log(T)
+console.log(typeof T)
+
+//3.undefine
+let b;
+console.log(typeof b)
+
+
+
